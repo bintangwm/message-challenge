6bde2faee14455720cd7b0dac0d1914bf257f7b8
Bintang Wibawa Mukti <bintang.wib@gmail.com>
2021-01-31 19:12:01 +0700

    add reply API 1. add route /reply/:id 2. add static method to reply at MessageController

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const userRouter = require('./users')
 const messageRouter = require('./message')
 const conversationRouter = require('./conversation')
 const participantRouter = require('./participant')
+const MessageController = require('../controllers/MessageController')
 
 router.use('/user', userRouter)
 router.use('/message', messageRouter)
+router.post('/reply/:id', MessageController.replyMessage)
 router.use('/participant', participantRouter)
 router.use('/conversation', conversationRouter)
 
